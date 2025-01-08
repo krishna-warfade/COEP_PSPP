0433e5377b824e5d6fa599d0d8f95d9deff53404
Krishna Warfade <krishnawarfade@gmail.com>
2025-01-08 21:59:04 +0530

    Add files via upload

---
+++ Assignment_Files/myscanf.c
@@ -0,0 +1,146 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int myscanf(char *format, void *arg)
+{
+	char ch;
+	char *p = arg;
+	int i, num, sign, conv;
+
+	i = 0;
+	num = 0;
+	conv = 0;
+	sign = 1;
+	if (*format == '%') {
+		format++;
+
+		if (*format == 'd') {
+			ch = getchar();
+			while ((ch == ' ' || ch == '\t' || ch == '\n'))
+				ch = getchar();
+
+			if (ch == '-') {
+				sign = -1;
+				ch = getchar();
+			} else if (ch == '+') {
+				ch = getchar();
+			}
+			while (ch >= '0' && ch <= '9') {
+				num = num * 10 + (ch - '0');
+				ch = getchar();
+				i++;
+			}
+			
+			if (i > 0) {
+                *(int *)p = sign * num;
+                conv++;
+            } else {
+                return 0;
+            }
+			if (ch == EOF) {
+				return -1;
+            }
+		} else if (*format == 'u') {
+			ch = getchar();
+
+			while ((ch == ' ' || ch == '\t' || ch == '\n'))
+				ch = getchar();
+
+			if (ch == '-')
+				return 0;
+			if (ch == '+')
+				ch = getchar();
+
+			while (ch >= '0' && ch <= '9') {
+				num = num * 10 + (ch - '0');
+				ch = getchar();
+				i++;
+			}
+			if (ch == EOF) {
+				*(unsigned int *)p = num;
+				return -1;
+			} else if (num) {
+				*(unsigned int *)p = (unsigned int)num;
+				conv++;
+			} else {
+				return 0;
+			}
+		} else if (*format == 'c') {
+			ch = getchar();
+			if (ch == EOF) {
+				*(char *)p = ch;
+				return -1;
+			}
+			*(char *)p = ch;
+			return 1;
+		} else if (*format == 'f') {
+			char num[128];
+			int j = 0;
+
+			ch = getchar();
+			while (ch == ' ' || ch == '\t' || ch == '\n')
+				ch = getchar();
+			while (ch != EOF && (ch != ' ' && ch != '\t' && ch != '\n')) {
+				num[j++] = ch;
+				ch = getchar();
+			}
+			num[j] = '\0';
+			float float_num;
+
+			float_num = atof(num);
+			if (ch == EOF) {
+				*(float *)p = float_num;
+				return -1;
+			} else if (float_num) {
+				*(float *)p = float_num;
+				conv++;
+			} else
+				return 0;
+		} else if (*format == 'l' && *(format + 1) == 'f') {
+			char num[128];
+			int j = 0;
+
+			ch = getchar();
+			while (ch == ' ' || ch == '\t' || ch == '\n')
+				ch = getchar();
+			while (ch != EOF && (ch != ' ' && ch != '\t' && ch != '\n')) {
+				num[j++] = ch;
+				ch = getchar();
+			}
+			num[j] = '\0';
+
+			double double_num;
+
+			double_num = atof(num);
+			if (ch == EOF) {
+				*(double *)p = double_num;
+				return -1;
+			} else if (double_num) {
+				*(double *)p = double_num;
+				conv++;
+			} else
+				return 0;
+		} else if (*format == 's') {
+			char str[1024];
+			int j = 0;
+
+			ch = getchar();
+			while (ch == ' ' || ch == '\t' || ch == '\n')
+				ch = getchar();
+
+			while (ch != EOF && (ch != ' ' && ch != '\t' && ch != '\n')) {
+				str[j++] = ch;
+				ch = getchar();
+			}
+			str[j] = '\0';
+			if (ch == EOF) {
+				return -1;
+			} else {
+				strcpy((char *)p, str);
+				conv++;
+			}
+		}
+	}
+	return conv;
+}
---
+++ Assignment_Files/palindrome.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+char make_lower(char ch)
+{	if (ch >= 'A' && ch <= 'Z')
+		return ch + 32;
+ 	return ch;
+}
+
+int is_pal(char *word)
+{
+	int len, i;
+
+	len = strlen(word);
+	if (len == 1)
+		return 1;
+
+	for (i = 0; i < len / 2; i++) {
+	    if (word[i] == ' ' || word[i] == '\n' || word[i] == '\t') {
+			return 0;
+		} else if (make_lower((word[i])) != make_lower(word[len - i - 1])) {
+			return 0;
+	    }
+		return 1;
+	}
+	return 0;
+}
+
+int main()
+{
+	int fd, i = 0;
+	char data[256], ch, filename[128];
+
+    scanf("%s", filename);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1) {
+		printf("open failed\n");
+		return 1;
+	}
+	while (read(fd, &ch, 1)) {
+		if (ch == ' ' || ch == '\n' || ch == '\t') {
+			data[i] = '\0'; // null terminating prev. word
+			if (is_pal(data))
+				printf("%s\n", data);
+			i = 0; // for new word
+		} else {
+			data[i] = ch;
+			i++;
+		}
+	}
+	if (i > 0) {
+	    data[i] = '\0';
+	    if (is_pal(data)) {
+	        printf("%s\n", data);
+	    }
+	}
+    close(fd);
+	return 0;
+}
