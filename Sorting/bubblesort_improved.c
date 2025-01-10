60a2c66a6400e4c1a15db903b810a610ca6a6419
Krishna Warfade <krishnawarfade@gmail.com>
2025-01-10 10:02:58 +0530

    Add files via upload

---
+++ Sorting/bubblesort_improved.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#define SIZE 16
+void swap (int a[16], int i, int j) {
+    int temp = a[i];
+    a[i] = a[j];
+    a[j] = temp;
+}
+int main()
+{
+    int a[SIZE] = {5, 4, 10, 1, 6, 2};
+    int len = 6;
+    int swapped = 0;
+    printf("Before Sorting : \n");
+    for(int i = 0; i < len; i++)
+        printf("%d ", a[i]);
+    printf("\n");
+    int i, j;
+    for(int i = 0; i < len - 1; i++) {
+        for(j = 0; j < len - i - 1; j++) {
+            if(a[j + 1] > a[j]) {
+                swap(a, j, j + 1);
+                swapped = 1;
+                }
+        }
+        if(!swapped)
+        	break;
+    }
+    printf("After Sorting : \n");
+    for(i = 0; i < len; i++)
+        printf("%d ", a[i]);
+    return 0;
+}
