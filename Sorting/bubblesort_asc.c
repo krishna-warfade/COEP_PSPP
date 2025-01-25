db9516bd6adb97e45123bc16e90d8ef5340d053a
Krishna Warfade <krishnawarfade@gmail.com>
2025-01-25 10:02:55 +0530

    Update bubblesort_asc.c

--- Sorting/bubblesort_asc.c
+++ Sorting/bubblesort_asc.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #define SIZE 16
+
 void swap (int a[16], int i, int j) {
     int temp = a[i];
     a[i] = a[j];
     a[j] = temp;
 }
-int main()
-{
+
+int main() {
     int a[SIZE] = {5, 4, 10, 1, 6, 2};
     int len = 6;
     printf("Before Sorting : \n");
@@ -16,10 +17,9 @@ int main()
     int i, j;
     for(int i = 0; i < len - 1; i++) {
         for(j = 0; j < len - i - 1; j++) {
-            if(a[j] > a[j + 1])
-                swap(a, j, j + 1);
+            if(a[j] > a[j + 1]) swap(a, j, j + 1);
         }
-            }
+    }
     printf("After Sorting : \n");
     for(i = 0; i < len; i++)
         printf("%d ", a[i]);
