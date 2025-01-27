20144618e41350c4e82a1c40a82d1a15a527e17c
Krishna Warfade <krishnawarfade@gmail.com>
2025-01-27 11:16:22 +0530

    Update matrix_addn.c

--- Lab ESE/Matrix Addition/matrix_addn.c
+++ Lab ESE/Matrix Addition/matrix_addn.c
@@ -7,9 +7,9 @@
 		/*EXTRACT LINE FROM FILE*/
 int readline(int fd, char *line, int size)
 {
-
 	char ch;
 	int len = 0;
+
 	while(read(fd, &ch, 1) && ch != '\n') {
 		line[len] = ch;
 		len++;
@@ -34,6 +34,7 @@ int insert_order(char *line, int *rows1, int *columns1)
 void insert_data(char *line, int *rows1, int *columns1, int i, int arr1[SIZE][SIZE])
 {
 	char *token;
+
 	token = strtok(line, "\t");
 	arr1[i][0] = atoi(token);
 	for (int j = 1; j < *columns1; j++) {
@@ -66,8 +67,10 @@ int readfile(char *filename, int *rows1, int *columns1, int arr1[SIZE][SIZE])
 		/*PRINT ARRAY*/
 void print_array(int arr[SIZE][SIZE], int rows, int columns)
 {
-	for (int i = 0; i < rows; i++) {
-		for (int j = 0; j < columns; j++)
+	int i, j;
+
+	for (i = 0; i < rows; i++) {
+		for (j = 0; j < columns; j++)
 			printf("%d ", arr[i][j]);
 		printf("\n");
 	}
@@ -75,8 +78,10 @@ void print_array(int arr[SIZE][SIZE], int rows, int columns)
 		/*FIND SUM OF 2 ARRAYS*/
 void find_sum(int arr1[SIZE][SIZE], int arr2[SIZE][SIZE], int sum[SIZE][SIZE], int rows, int columns)
 {
-	for (int i = 0; i < rows; i++) {
-		for (int j = 0; j < columns; j++)
+	int i, j;
+
+	for (i = 0; i < rows; i++) {
+		for (j = 0; j < columns; j++)
 			sum[i][j] = arr1[i][j] + arr2[i][j];
 	}
 }
@@ -89,8 +94,10 @@ int main(int argc, char *argv[])
 	int columns2 = 0;
 	int arr1[SIZE][SIZE], arr2[SIZE][SIZE], sum[SIZE][SIZE];
 
+	/*READ THE 2 FILES WHICH CONSIST OF MATRICES*/
 	readfile(argv[1], &rows1, &columns1, arr1);
 	readfile(argv[2], &rows2, &columns2, arr2);
+	/*PRINT THE 2 MATRICES, READ FROM THE FILE*/
 	print_array(arr1, rows1, columns1);
 	printf("----------------------\n");
 	print_array(arr2, rows2, columns2);
@@ -98,9 +105,11 @@ int main(int argc, char *argv[])
 		printf("Addition not compatible\n\n");
 		return 1;
 	}
+	/*FUNCTION TO FIND SUM, IF ADDITION IS COMPATIBLE*/
 	find_sum(arr1, arr2, sum, rows1, columns1);
 	printf("----------------------\n");
 	printf("SUM IS :\n");
+	/*PRINT THE SUM OF 2 MATRICES*/
 	print_array(sum, rows1, columns1);
 	return 0;
 }
