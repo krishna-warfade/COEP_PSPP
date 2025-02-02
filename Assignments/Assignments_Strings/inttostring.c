199b0ad8e300618e0d71ecc467370a7b895d5c27
Krishna Warfade <krishnawarfade@gmail.com>
2025-02-02 19:18:47 +0530

    Update inttostring.c

--- Assignments/Assignments_Strings/inttostring.c
+++ Assignments/Assignments_Strings/inttostring.c
@@ -1,7 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
+
 char *inttostring(int x) {
     int num, dig = 0, i = 0, len;
+
     num = x;
     char *p;
     while(num != 0) {
@@ -11,6 +13,7 @@ char *inttostring(int x) {
     if(x < 0) {
         len = dig + 2;
         p = (char *)malloc(len * sizeof(char));
+
         x *= -1;
         p[0] = '-';
         i = len - 2;
@@ -22,6 +25,7 @@ char *inttostring(int x) {
     }
     if(x == 0) {
         p = (char *)malloc(2 * sizeof(char));
+
         p[0] = '0';
         p[1] = '\0';
         return p;
@@ -33,8 +37,7 @@ char *inttostring(int x) {
     p[len - 1] = '\0';
     return p;
 }
-int main()
-{
+int main() {
 	printf("%s\n", inttostring(-964));
-return 0;
+	return 0;
 }
