3699df0dd37969613067268fe12c5f4119ee0a30
Krishna Warfade <krishnawarfade@gmail.com>
2025-02-02 19:25:26 +0530

    Update cosx.c

--- Assignments/Assignment_If-Else-2/cosx.c
+++ Assignments/Assignment_If-Else-2/cosx.c
@@ -1,24 +1,20 @@
 #include <stdio.h>
 #include <math.h>
-#define pi 3.14159265359
+#define PI 3.14159265359
 
-int main()
-{
+int main() {
 	double x;
 	int sign = -1, i = 2;
 	float cosx = 1.0, term = 1.0, precision = 1e-6;
 
 	scanf("%lf", &x);
-	while (x > pi)
-	{
-		x = x - 2 * pi;
-	}
-	while (fabs(term) > precision)
-	{
+	while (x > PI)
+		x -= 2 * PI;
+	while (fabs(term) > precision) {
 		term = (term * (x * x)) / ((i) * (i - 1));
-		cosx = cosx + (sign * term);
-		sign = (sign * (-1));
-		i = i + 2;
+		cosx += (sign * term);
+		sign *= -1;
+		i += 2;
 	}
 	printf("%.4lf", cosx);
 	return 0;
